c56bf15352c9d8b8fb26ebf2c152e32ec4b291fa
Adina Pusok <adina.pusok@gmail.com>
2025-05-04 23:36:10 +0100

    Part4

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,38 +1,18 @@
 export const nav = [
-/* 	{
+	{
 		title: 'Home',
 		slug: '/',
-	}, */
-	{
-		title: 'About',
-		slug: '/apusok/about',
 	},
-/* 	{
+	{
 		title: 'Blog',
 		slug: '/blog',
-	}, */
-	{
-		title: 'Team',
-		slug: '/apusok/team',
 	},
 	{
-		title: 'Research',
-		slug: '/apusok/research',
+		title: 'About',
+		slug: '/company/about',
 	},
 	{
-		title: 'Software',
-		slug: '/apusok/software',
-	},
-/* 	{
-		title: 'Teaching',
-		slug: '/apusok/teaching',
-	}, */
-	{
-		title: 'Publications',
-		slug: '/apusok/publications',
-	},
-/* 	{
 		title: 'Contact',
 		slug: '/company/contact',
-	}, */
+	},
 ];
